122d7db5e87c39c744b755a337a9f8d0618a400f
Nikhil Patel <nikhilpatel26150@gmail.com>
2025-03-25 10:33:06 +0530

    footer created

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='flex justify-center items-center px-4 py-6 text-xl font-bold '>All right reserved @2025 : LaceUp Hub</div>
+    <>
+      <div className='mb-10 m-5'>
+        <h2 className='font-bold'>LaceUp Hub</h2>
+        
+
+      </div>
+      <div className='flex justify-center items-center px-4 py-6 text-xl font-bold '>All right reserved @2025 : LaceUp Hub</div>
+    </>
   )
 }
 
