06fb1de7c49259b56cbfb7cfc7daaa32770f7492
Nikhil Patel <nikhilpatel26150@gmail.com>
2025-03-27 13:29:34 +0530

    edit cards to GEarup

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import Cards from '../components/Cards'
 import Hero from '../components/Hero'
 import Suggestions from '../components/Suggestions'
 import Category from '../components/Category'
+import GearUp from '../components/GearUp'
 
 const Home = () => {
   return (
     <>
       <Hero />
       <Category/>
-      <Suggestions />
-      <Cards />
+      {/* <Suggestions /> */}
+      <GearUp />
     </>
   )
 }
