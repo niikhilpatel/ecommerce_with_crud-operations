806c2149dde7f9def777a03978f50ec2ae0ca3a4
Nikhil Patel <nikhilpatel26150@gmail.com>
2025-04-01 10:42:13 +0530

    user updated

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -9,14 +9,13 @@ const Navbar = () => {
         <div className="flex justify-between p-4">
             <div className="flex ">
                 <img onClick={() => navigate("/")} src={Logo} alt="logo" className="w-40 h-20 pb-2 rounded-md"/>
-                {/* <a className="font-bold text-4xl">LaceUp Hub</a> */}
             </div>
             <div className="mr-8 mt-2">
                 <button
                     className="bg-gray-900 text-white py-2 px-3 rounded-lg"
                     onClick={() => navigate("/sign-in")}
                 >
-                    Sign in
+                    User not login
                 </button>
             </div>
         </div>
